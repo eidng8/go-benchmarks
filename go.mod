43f0e2461fdbaab17e91fa7d37ddec7e2bc13086
eidng8 <jackey.cheung@qq.com>
2025-02-13 10:08:12 +0800

    add string benchmarks

--- go.mod
+++ go.mod
@@ -5,14 +5,18 @@ go 1.23.2
 require (
 	github.com/bytedance/sonic v1.12.7
 	github.com/json-iterator/go v1.1.12
+	github.com/stretchr/testify v1.8.1
 )
 
 require (
 	github.com/bytedance/sonic/loader v0.2.2 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ string_test.go
@@ -0,0 +1,115 @@
+package benchmarks
+
+// see also https://dev.to/pmalhaire/concatenate-strings-in-golang-a-quick-benchmark-4ahh
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+var (
+	shortStr = "short"
+	shortLen = len(shortStr)
+	longStr  = strings.Repeat("long ", 100)
+	longLen  = len(longStr)
+)
+
+func Benchmark_string_concat_short(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = shortStr + shortStr
+	}
+}
+
+func Benchmark_string_sprintf_short(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = fmt.Sprintf("%s%d", shortStr, 1)
+	}
+}
+
+func Benchmark_string_concat_conv_short(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = shortStr + strconv.Itoa(1)
+	}
+}
+
+func Benchmark_string_builder_short(b *testing.B) {
+	b.StopTimer()
+	var sb strings.Builder
+	sb.Grow(shortLen * b.N)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sb.WriteString(shortStr)
+	}
+}
+
+func Benchmark_string_builder_fprintf_short(b *testing.B) {
+	b.StopTimer()
+	var sb strings.Builder
+	sb.Grow(shortLen * b.N)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = fmt.Fprintf(&sb, "%s%d", shortStr, 1)
+	}
+}
+
+func Benchmark_string_builder_conv_short(b *testing.B) {
+	b.StopTimer()
+	var sb strings.Builder
+	sb.Grow((shortLen + 1) * b.N)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sb.WriteString(shortStr)
+		sb.WriteString(strconv.Itoa(1))
+	}
+}
+
+func Benchmark_string_concat_long(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = longStr + longStr
+	}
+}
+
+func Benchmark_string_sprintf_long(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = fmt.Sprintf("%s%d", longStr, 1)
+	}
+}
+
+func Benchmark_string_concat_conv_long(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = longStr + strconv.Itoa(1)
+	}
+}
+
+func Benchmark_string_builder_long(b *testing.B) {
+	b.StopTimer()
+	var sb strings.Builder
+	sb.Grow(longLen * b.N)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sb.WriteString(longStr)
+	}
+}
+
+func Benchmark_string_builder_fprintf_long(b *testing.B) {
+	b.StopTimer()
+	var sb strings.Builder
+	sb.Grow(longLen * b.N)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = fmt.Fprintf(&sb, "%s%d", longStr, 1)
+	}
+}
+
+func Benchmark_string_builder_conv_long(b *testing.B) {
+	b.StopTimer()
+	var sb strings.Builder
+	sb.Grow((longLen + 1) * b.N)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		sb.WriteString(longStr)
+		sb.WriteString(strconv.Itoa(1))
+	}
+}
